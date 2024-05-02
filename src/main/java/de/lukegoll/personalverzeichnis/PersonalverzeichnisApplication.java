21158ac138174631489e7566e05238f6ca2ebd2f
Luke Gollenstede <lukegollenstede@gmail.com>
2024-05-02 17:04:13 +0200

    Initial commit

---
+++ src/main/java/de/lukegoll/personalverzeichnis/PersonalverzeichnisApplication.java
@@ -0,0 +1,13 @@
+package de.lukegoll.personalverzeichnis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalverzeichnisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalverzeichnisApplication.class, args);
+	}
+
+}
