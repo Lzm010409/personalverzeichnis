209bf5cb233bb3f4eb235537aba9fde1bac1f744
daveh <davehd75@gmail.com>
2024-05-11 15:16:54 +0200

    Ich habe dasdads

--- src/main/java/de/lukegoll/personalverzeichnis/web/controller/HomeController.java
+++ src/main/java/de/lukegoll/personalverzeichnis/web/controller/HomeController.java
@@ -2,6 +2,10 @@ package de.lukegoll.personalverzeichnis.web.controller;
 
 import java.util.LinkedList;
 
+import de.lukegoll.personalverzeichnis.domain.entities.CapabilityType;
+import de.lukegoll.personalverzeichnis.domain.services.CapabilityService;
+import de.lukegoll.personalverzeichnis.domain.services.CapabilityTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +17,17 @@ import jakarta.annotation.Generated;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	private final CapabilityTypeService capabilityTypeService;
 
-	
+	@Autowired
+	public HomeController(CapabilityTypeService capabilityTypeService) {
+		this.capabilityTypeService = capabilityTypeService;
+	}
+
 	@GetMapping("")
 	public String getHome(Model model) {
-		
-		
+		capabilityTypeService.save(new CapabilityType());
+		capabilityTypeService.findAll();
 		model.addAttribute("employees", new LinkedList<Employee>());
 		
 		return "content/home";
